87b41b1290d707ac22d9b82555352e8a49e5a9dd
zohaib304 <zohaibtariq304@gmail.com>
2022-05-25 20:48:38 +0500

    solved

--- 01_fizzbuzz.js
+++ 01_fizzbuzz.js
@@ -1,17 +1,30 @@
 fizzBuzz = function(num) {
-// write a function that receives a number as its argument;
-// if the number is divisible by 3, the function should return 'fizz';
-// if the number is divisible by 5, the function should return 'buzz';
-// if the number is divisible by 3 and 5, the function should return
-// 'fizzbuzz';
-//
-// otherwise the function should return the number, or false if no number
-// was provided or the value provided is not a number
+    // write a function that receives a number as its argument;
+    // if the number is divisible by 3, the function should return 'fizz';
+    // if the number is divisible by 5, the function should return 'buzz';
+    // if the number is divisible by 3 and 5, the function should return
+    // 'fizzbuzz';
+    //
+    // otherwise the function should return the number, or false if no number
+    // was provided or the value provided is not a number
+    if (typeof num === 'number') {
+        if (num % 3 === 0 && num % 5 === 0) {
+            return 'fizzbuzz';
+        } else if (num % 3 === 0) {
+            return 'fizz';
+        } else if (num % 5 === 0) {
+            return 'buzz';
+        } else {
+            return num;
+        }
+    } else {
+        return false;
+    }
 }
 
 // TEST CASES will be provided by the test runner
 console.log('fizzBuzz(undefined) === false :', fizzBuzz(undefined) === false);
-console.log('fizzBuzz(foo) === false :', fizzBuzz(foo) === false);
+// console.log('fizzBuzz(foo) === false :', fizzBuzz(foo) === false);
 console.log('fizzBuzz(22) === 22 :', fizzBuzz(22) === 22);
 console.log('fizzBuzz(33) === "fizz" :', fizzBuzz(33) === "fizz");
 console.log('fizzBuzz(55) === "buzz" :', fizzBuzz(55) === "buzz");
