595d718d48d4290591e4bbcd8eb9b2ea19d33919
Muhammad zohaib <58664161+devzohaib@users.noreply.github.com>
2022-05-25 10:57:30 +0000

    test cased added

--- 01_fizzbuzz.js
+++ 01_fizzbuzz.js
@@ -7,6 +7,13 @@ fizzBuzz = function(num) {
 //
 // otherwise the function should return the number, or false if no number
 // was provided or the value provided is not a number
+ return 3;
 }
 
 // TEST CASES will be provided by the test runner
+console.log('fizzBuzz(undefined) === false :', fizzBuzz(undefined) === false);
+console.log('fizzBuzz(foo) === false :', fizzBuzz(foo) === false);
+console.log('fizzBuzz(22) === 22 :', fizzBuzz(22) === 22);
+console.log('fizzBuzz(33) === "fizz" :', fizzBuzz(33) === "fizz");
+console.log('fizzBuzz(55) === "buzz" :', fizzBuzz(55) === "buzz");
+console.log('fizzBuzz(150) === "fizzbuzz" :', fizzBuzz(150) === "fizzbuzz");
